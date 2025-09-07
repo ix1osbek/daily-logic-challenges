a4f35280ac6ba389a0794fa6547e48401480f69b
ix1osbek <bekerkinov2004@gmail.com>
2025-09-08 03:25:57 +0500

    ./

--- week-1/day-1.js
+++ week-1/day-1.js
@@ -31,3 +31,19 @@
 // }
 
 // console.log(sumEven([1, 2, 3, 4, 5, 6, 6]));
+
+
+////// matter -5
+// function findDuplicates(array) {
+//     let duplicates = [];
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[i] === array[j] && !duplicates.includes(array[i])) {
+//                 duplicates.push(array[i]);
+//             }
+//         }
+//     }
+//     return duplicates;
+// }
+
+// console.log(findDuplicates([1, 1, 22,22, 3, 4]));
